d4481ff5fad25e861a64fae817ec2fea7cb771dd
Noah Wardlow <noah@inkind.com>
2022-05-30 19:41:35 -0500

    angles

--- client/src/components/GUI/ControlPanel.js
+++ client/src/components/GUI/ControlPanel.js
@@ -1,14 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { defaultRobotState } from "../../iniitalRobot";
 import Slider from "@mui/material/Slider";
 import { SocketContext } from "../../context/socket";
 import useAngles from "../../hooks/useAngles";
+import Kinematic from "../../helpers/inverseKinematics";
 
 const ControlPanel = () => {
   let jointLimits = Object.values(defaultRobotState.jointLimits);
   let angles = useAngles();
+  let IK;
+  IK = new Kinematic(
+    Object.values(defaultRobotState.geometry).map((v) => [v.x, v.y, v.z])
+  );
 
   const socket = useContext(SocketContext);
+  let [IKMode, setIKMode] = useState(false);
 
   function setAngle(jointNumber, value) {
     const angleState = angles;
@@ -16,16 +22,47 @@ const ControlPanel = () => {
     socket.emit("angleState", angleState);
   }
 
+  let [points] = useState({ x: 10, y: 10, z: 10, pitch: Math.PI, yaw: 0, roll:0 });
+
+  function setAnglesFromIK() {
+    IK.calculateAngles(points.x, points.y, points.z, points.pitch, points.yaw, points.roll, angles);
+  }
+
+  useEffect(() => {
+    if (IKMode) {
+      IK.calculateAngles(points.x, points.y, points.z, points.pitch, points.yaw, points.roll, angles);
+    }
+  }, [IKMode]);
 
   return (
-    <section className="w-full min-h-full p-8 bg-black text-white bg-opacity-90 control-panel grid grid-cols-2 gap-8">
+    <section className="w-full min-h-full p-8 bg-black text-white bg-opacity-90 control-panel grid md:grid-cols-2 gap-8">
       <div className="flex flex-col gap-2">
-        <h5 className="font-bold">Joint Angles</h5>
-        {angles &&
+        <div className="flex gap-2 w-full pb-4">
+          <button
+            onClick={() => setIKMode(false)}
+            className={`font-bold border-white rounded-full border w-full p-2 ${
+              !IKMode ? "bg-white text-black" : ""
+            }`}
+          >
+            Joint Angles
+          </button>
+          <button
+            onClick={() => setIKMode(true)}
+            className={`font-bold border-white rounded-full border w-full p-2 ${
+              IKMode ? "bg-white text-black" : ""
+            }`}
+          >
+            Inverse Kinematics
+          </button>
+        </div>
+        {!IKMode &&
+          angles &&
           jointLimits?.slice(0, 5).map((joint, i) => {
             return (
               <div className="w-full max-w-lg flex gap-1 items-center" key={i}>
-                <span className="text-sm w-16">Joint {i + 1}</span>
+                <span className="text-sm w-16 font-semibold">
+                  Joint {i + 1}
+                </span>
                 <Slider
                   aria-label="Small steps"
                   value={angles[i]}
@@ -34,6 +71,44 @@ const ControlPanel = () => {
                   max={joint[1]}
                   onChange={({ target }) => {
                     setAngle(i, target.value);
+                  }}
+                />
+              </div>
+            );
+          })}
+        {IKMode &&
+          ["x", "y", "z"].map((direction, i) => {
+            return (
+              <div className="w-full max-w-lg flex gap-1 items-center" key={i}>
+                <span className="text-sm w-16 font-semibold">{direction}</span>
+                <Slider
+                  aria-label="Small steps"
+                  defaultValue={10}
+                  step={1}
+                  min={-25}
+                  max={25}
+                  onChange={({ target }) => {
+                    points[direction] = target.value;
+                    setAnglesFromIK();
+                  }}
+                />
+              </div>
+            );
+          })}
+          {IKMode &&
+          ["pitch", "yaw"].map((direction, i) => {
+            return (
+              <div className="w-full max-w-lg flex gap-1 items-center" key={i}>
+                <span className="text-sm w-16 font-semibold">{direction}</span>
+                <Slider
+                  aria-label="Small steps"
+                  defaultValue={0}
+                  step={.1}
+                  min={-3.14}
+                  max={3.14}
+                  onChange={({ target }) => {
+                    points[direction] = target.value;
+                    setAnglesFromIK();
                   }}
                 />
               </div>
